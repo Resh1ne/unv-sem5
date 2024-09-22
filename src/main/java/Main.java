24105f3c7ae338e83df86b52ac280ad248a5b322
User <daniilpototskiy@mail.ru>
2024-09-22 22:29:31 +0300

    Added main

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+
+        BrightCorrection br = new BrightCorrection();
+        br.startProcess("image1.jpg","image2.jpg");
+    }
+}
