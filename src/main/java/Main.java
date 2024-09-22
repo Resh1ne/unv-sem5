f5d9b33d8447c2c0e0410686326efea24bb284e1
User <daniilpototskiy@mail.ru>
2024-09-22 23:50:46 +0300

    reformated code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        FFTExample fftExample = new FFTExample(); // Создание объекта FFTExample
-        fftExample.startProgram(); // Запуск программы
+        FFTExample fftExample = new FFTExample();
+        fftExample.startProgram();
     }
 }
