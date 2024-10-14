000b0b8113bdb5153fac9e0a6d4edc5479275f28
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-10-14 23:38:03 +0300

    Completed ArtistDaoImpl

--- src/main/java/by/bsuir/pbz2/data/impl/ArtistDaoImpl.java
+++ src/main/java/by/bsuir/pbz2/data/impl/ArtistDaoImpl.java
@@ -4,6 +4,13 @@ import by.bsuir.pbz2.data.ArtistDao;
 import by.bsuir.pbz2.data.connection.DataSource;
 import by.bsuir.pbz2.data.entity.Artist;
 
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArtistDaoImpl implements ArtistDao {
@@ -20,26 +27,101 @@ public class ArtistDaoImpl implements ArtistDao {
 
     @Override
     public Artist create(Artist entity) {
-        return null;
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(CREATION_QUERY, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, entity.getName());
+            statement.setString(2, entity.getBirthPlace());
+            statement.setDate(3, Date.valueOf(entity.getBirthDate()));
+            statement.setString(4, entity.getBiography());
+            statement.setString(5, entity.getEducation());
+            statement.executeUpdate();
+            ResultSet keys = statement.getGeneratedKeys();
+            if (keys.next()) {
+                long id = keys.getLong("id");
+                return findById(id);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        throw new RuntimeException("Can't create artist: " + entity);
     }
 
     @Override
     public Artist findById(Long id) {
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_QUERY);
+            statement.setLong(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return mapRow(resultSet);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
+    }
+
+    private static Artist mapRow(ResultSet resultSet) throws SQLException {
+        Artist artist = new Artist();
+        artist.setId(resultSet.getLong("id"));
+        artist.setName(resultSet.getString("name"));
+        artist.setBirthPlace(resultSet.getString("birth_place"));
+        artist.setBirthDate(resultSet.getDate("birth_date").toLocalDate());
+        artist.setBiography(resultSet.getString("biography"));
+        artist.setEducation(resultSet.getString("education"));
+        return artist;
     }
 
     @Override
     public List<Artist> findAll() {
-        return null;
+        List<Artist> artists = new ArrayList<>();
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(FIND_ALL_QUERY);
+            while (resultSet.next()) {
+                Artist artist = mapRow(resultSet);
+                artists.add(artist);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return artists;
     }
 
     @Override
     public Artist update(Artist entity) {
-        return null;
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(UPDATE_QUERY);
+            statement.setString(1, entity.getName());
+            statement.setString(2, entity.getBirthPlace());
+            statement.setDate(3, Date.valueOf(entity.getBirthDate()));
+            statement.setString(4, entity.getBiography());
+            statement.setString(5, entity.getEducation());
+            statement.setLong(6, entity.getId());
+
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                return findById(entity.getId());
+            } else {
+                throw new RuntimeException("Failed to update artist. No rows affected.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public boolean delete(Long id) {
-        return false;
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(DELETE_QUERY);
+            statement.setLong(1, id);
+            int rowsAffected = statement.executeUpdate();
+
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
