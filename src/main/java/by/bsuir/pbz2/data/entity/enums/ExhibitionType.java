a608598a9f0bd3fcd78f561629c1ef03e943ebb9
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-10-14 20:20:30 +0300

    Added enum classes

---
+++ src/main/java/by/bsuir/pbz2/data/entity/enums/ExhibitionType.java
@@ -0,0 +1,8 @@
+package by.bsuir.pbz2.data.entity.enums;
+
+@SuppressWarnings("unused")
+public enum ExhibitionType {
+    VISUAL_ART,
+    APPLIED_ART,
+    SCULPTURE
+}
---
+++ src/main/java/by/bsuir/pbz2/data/entity/enums/OwnerType.java
@@ -0,0 +1,9 @@
+package by.bsuir.pbz2.data.entity.enums;
+
+@SuppressWarnings("unused")
+public enum OwnerType {
+    CITY_ORGANIZATION,
+    REGIONAL_ORGANIZATION,
+    PUBLIC_ORGANIZATION,
+    PRIVATE_INDIVIDUAL
+}
