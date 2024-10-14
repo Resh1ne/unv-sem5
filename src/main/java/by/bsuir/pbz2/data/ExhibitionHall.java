f51e25ec5085cc5028f7da478d37de36004072af
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-10-14 21:58:26 +0300

    renamed class

--- src/main/java/by/bsuir/pbz2/data/ExhibitionHall.java
+++
@@ -1,4 +0,0 @@
-package by.bsuir.pbz2.data;
-
-public interface ExhibitionHall extends CrudDao<Long, by.bsuir.pbz2.data.entity.ExhibitionHall> {
-}
---
+++ src/main/java/by/bsuir/pbz2/data/ExhibitionHallDao.java
@@ -0,0 +1,4 @@
+package by.bsuir.pbz2.data;
+
+public interface ExhibitionHallDao extends CrudDao<Long, by.bsuir.pbz2.data.entity.ExhibitionHall> {
+}
