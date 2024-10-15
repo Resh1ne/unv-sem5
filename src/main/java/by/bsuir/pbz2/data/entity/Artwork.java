1978dbcfa300c89c42223319c82c2c09e896b507
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-10-15 20:47:22 +0300

    Added lombok

--- src/main/java/by/bsuir/pbz2/data/entity/Artwork.java
+++ src/main/java/by/bsuir/pbz2/data/entity/Artwork.java
@@ -1,12 +1,16 @@
 package by.bsuir.pbz2.data.entity;
 
 import by.bsuir.pbz2.data.entity.enums.ExecutionType;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Objects;
 
 
+@Setter
+@Getter
 public class Artwork {
     private Long id;
     private String title;
@@ -15,83 +19,19 @@ public class Artwork {
     private BigDecimal height;
     private BigDecimal width;
     private BigDecimal volume;
-    private Artist artistId;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public ExecutionType getExecutionType() {
-        return executionType;
-    }
-
-    public void setExecutionType(ExecutionType executionType) {
-        this.executionType = executionType;
-    }
-
-    public LocalDate getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDate creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public BigDecimal getHeight() {
-        return height;
-    }
-
-    public void setHeight(BigDecimal height) {
-        this.height = height;
-    }
-
-    public BigDecimal getWidth() {
-        return width;
-    }
-
-    public void setWidth(BigDecimal width) {
-        this.width = width;
-    }
-
-    public BigDecimal getVolume() {
-        return volume;
-    }
-
-    public void setVolume(BigDecimal volume) {
-        this.volume = volume;
-    }
-
-    public Artist getArtistId() {
-        return artistId;
-    }
-
-    public void setArtistId(Artist artistId) {
-        this.artistId = artistId;
-    }
+    private Artist artist;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Artwork artwork = (Artwork) o;
-        return Objects.equals(id, artwork.id) && Objects.equals(title, artwork.title) && executionType == artwork.executionType && Objects.equals(creationDate, artwork.creationDate) && Objects.equals(height, artwork.height) && Objects.equals(width, artwork.width) && Objects.equals(volume, artwork.volume) && Objects.equals(artistId, artwork.artistId);
+        return Objects.equals(id, artwork.id) && Objects.equals(title, artwork.title) && executionType == artwork.executionType && Objects.equals(creationDate, artwork.creationDate) && Objects.equals(height, artwork.height) && Objects.equals(width, artwork.width) && Objects.equals(volume, artwork.volume) && Objects.equals(artist, artwork.artist);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, title, executionType, creationDate, height, width, volume, artistId);
+        return Objects.hash(id, title, executionType, creationDate, height, width, volume, artist);
     }
 
     @Override
@@ -104,7 +44,7 @@ public class Artwork {
                 ", height=" + height +
                 ", width=" + width +
                 ", volume=" + volume +
-                ", artistId=" + artistId +
+                ", artistId=" + artist +
                 '}';
     }
 }
