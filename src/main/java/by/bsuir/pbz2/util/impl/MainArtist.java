a27eb096c452f967a3dd6a4f2c9ba7e6ee3297cd
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-10-15 22:21:59 +0300

    renamed Main class

--- src/main/java/by/bsuir/pbz2/util/impl/MainArtist.java
+++ src/main/java/by/bsuir/pbz2/util/impl/MainArtist.java
@@ -14,7 +14,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Main {
+public class MainArtist {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Pattern pattern = Pattern.compile("\\d+");
