34ea5b7c9aeea41d6b19c5065ef848e12b189365
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-10-15 21:06:08 +0300

    Added lombok

--- src/main/java/by/bsuir/pbz2/data/entity/Exhibition.java
+++ src/main/java/by/bsuir/pbz2/data/entity/Exhibition.java
@@ -1,78 +1,34 @@
 package by.bsuir.pbz2.data.entity;
 
 import by.bsuir.pbz2.data.entity.enums.ExhibitionType;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.Objects;
 
 
+@Setter
+@Getter
 public class Exhibition {
     private Long id;
     private String name;
-    private ExhibitionHall exhibitionHallId;
-    private ExhibitionType exhibitionType;
+    private ExhibitionHall hallId;
+    private ExhibitionType type;
     private LocalDate startDate;
     private LocalDate endDate;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public ExhibitionHall getExhibitionHallId() {
-        return exhibitionHallId;
-    }
-
-    public void setExhibitionHallId(ExhibitionHall exhibitionHallId) {
-        this.exhibitionHallId = exhibitionHallId;
-    }
-
-    public ExhibitionType getExhibitionType() {
-        return exhibitionType;
-    }
-
-    public void setExhibitionType(ExhibitionType exhibitionType) {
-        this.exhibitionType = exhibitionType;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Exhibition that = (Exhibition) o;
-        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(exhibitionHallId, that.exhibitionHallId) && exhibitionType == that.exhibitionType && Objects.equals(startDate, that.startDate) && Objects.equals(endDate, that.endDate);
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(hallId, that.hallId) && type == that.type && Objects.equals(startDate, that.startDate) && Objects.equals(endDate, that.endDate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name, exhibitionHallId, exhibitionType, startDate, endDate);
+        return Objects.hash(id, name, hallId, type, startDate, endDate);
     }
 
     @Override
@@ -80,8 +36,8 @@ public class Exhibition {
         return "Exhibition{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
-                ", exhibitionHallId=" + exhibitionHallId +
-                ", exhibitionType=" + exhibitionType +
+                ", exhibitionHallId=" + hallId +
+                ", exhibitionType=" + type +
                 ", startDate=" + startDate +
                 ", endDate=" + endDate +
                 '}';
