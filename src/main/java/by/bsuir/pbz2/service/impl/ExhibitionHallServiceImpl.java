0798f754fb9ac03997444eba03e5d16852c1fd57
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-11-08 00:12:36 +0300

    Implement ExhibitionHallService

---
+++ src/main/java/by/bsuir/pbz2/service/impl/ExhibitionHallServiceImpl.java
@@ -0,0 +1,78 @@
+package by.bsuir.pbz2.service.impl;
+
+import by.bsuir.pbz2.data.ExhibitionHallDao;
+import by.bsuir.pbz2.data.entity.ExhibitionHall;
+import by.bsuir.pbz2.service.ExhibitionHallService;
+import by.bsuir.pbz2.service.dto.ExhibitionHallDto;
+
+import java.util.List;
+
+public class ExhibitionHallServiceImpl implements ExhibitionHallService {
+    private final ExhibitionHallDao exhibitionHallDao;
+
+    public ExhibitionHallServiceImpl(ExhibitionHallDao exhibitionHallDao) {
+        this.exhibitionHallDao = exhibitionHallDao;
+    }
+
+    @Override
+    public ExhibitionHallDto create(ExhibitionHallDto dto) {
+        ExhibitionHall exhibitionHall = toExhibitionHallEntity(dto);
+        ExhibitionHall exhibitionHallCreated = exhibitionHallDao.create(exhibitionHall);
+        return toExhibitionHallDto(exhibitionHallCreated);
+    }
+
+    private ExhibitionHall toExhibitionHallEntity(ExhibitionHallDto dto) {
+        ExhibitionHall exhibitionHall = new ExhibitionHall();
+        exhibitionHall.setName(dto.getName());
+        exhibitionHall.setArea(dto.getArea());
+        exhibitionHall.setAddress(dto.getAddress());
+        exhibitionHall.setPhone(dto.getPhone());
+        exhibitionHall.setOwnerId(dto.getOwnerId());
+        return exhibitionHall;
+    }
+
+    private ExhibitionHallDto toExhibitionHallDto(ExhibitionHall entity) {
+        ExhibitionHallDto exhibitionHallDto = new ExhibitionHallDto();
+        exhibitionHallDto.setId(entity.getId());
+        exhibitionHallDto.setName(entity.getName());
+        exhibitionHallDto.setArea(entity.getArea());
+        exhibitionHallDto.setAddress(entity.getAddress());
+        exhibitionHallDto.setPhone(entity.getPhone());
+        exhibitionHallDto.setOwnerId(entity.getOwnerId());
+        return exhibitionHallDto;
+    }
+
+    @Override
+    public ExhibitionHallDto getById(Long id) {
+        ExhibitionHall exhibitionHall = exhibitionHallDao.findById(id);
+        if (exhibitionHall == null) {
+            throw new RuntimeException("No exhibition hall with id: " + id);
+        }
+        return toExhibitionHallDto(exhibitionHall);
+    }
+
+    @Override
+    public List<ExhibitionHallDto> getAll() {
+        return exhibitionHallDao.findAll()
+                .stream()
+                .map(this::toExhibitionHallDto)
+                .toList();
+    }
+
+    @Override
+    public ExhibitionHallDto update(ExhibitionHallDto dto) {
+        ExhibitionHall exhibitionHall = toExhibitionHallEntity(dto);
+        exhibitionHall.setId(dto.getId());
+        ExhibitionHall exhibitionHallCreated = exhibitionHallDao.update(exhibitionHall);
+        return toExhibitionHallDto(exhibitionHallCreated);
+    }
+
+    @Override
+    public void delete(Long id) {
+        ExhibitionHall exhibitionHall = exhibitionHallDao.findById(id);
+        if (exhibitionHall == null) {
+            throw new RuntimeException("Exhibition hall with id: " + id + " not found");
+        }
+        exhibitionHallDao.delete(id);
+    }
+}
