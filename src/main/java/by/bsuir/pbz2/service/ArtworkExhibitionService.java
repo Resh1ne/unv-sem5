a3d3e1711e2a58cfae0ea7226240e0a7a362b244
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-11-07 19:36:26 +0300

    Updated ArtworkExhibitionService

--- src/main/java/by/bsuir/pbz2/service/ArtworkExhibitionService.java
+++ src/main/java/by/bsuir/pbz2/service/ArtworkExhibitionService.java
@@ -1,17 +1,17 @@
 package by.bsuir.pbz2.service;
 
-import by.bsuir.pbz2.service.dto.ArtworkDto;
+import by.bsuir.pbz2.data.entity.Artwork;
+import by.bsuir.pbz2.data.entity.Exhibition;
 import by.bsuir.pbz2.service.dto.ArtworkExhibitionDto;
-import by.bsuir.pbz2.service.dto.ExhibitionDto;
 
 import java.util.List;
 
 public interface ArtworkExhibitionService {
     void create(ArtworkExhibitionDto dto);
 
-    ArtworkExhibitionDto getByExhibitionArtworkId(ArtworkDto artworkId, ExhibitionDto exhibitionId);
+    ArtworkExhibitionDto getByExhibitionArtworkId(Artwork artworkId, Exhibition exhibitionId);
 
     List<ArtworkExhibitionDto> getAll();
 
-    void delete(Long id);
+    void delete(ArtworkExhibitionDto artworkExhibitionDto);
 }
