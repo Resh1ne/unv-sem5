222608da5d51cec12209d67bc5076d27659cb1a8
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-11-08 20:57:53 +0300

    Changed class MainArtworkExhibition.java

--- src/main/java/by/bsuir/pbz2/util/impl/MainArtworkExhibition.java
+++ src/main/java/by/bsuir/pbz2/util/impl/MainArtworkExhibition.java
@@ -1,12 +1,16 @@
 package by.bsuir.pbz2.util.impl;
 
-import by.bsuir.pbz2.data.dao.ArtworkExhibitionDao;
 import by.bsuir.pbz2.data.connection.DataSource;
 import by.bsuir.pbz2.data.connection.impl.DataSourceImpl;
+import by.bsuir.pbz2.data.dao.ArtworkDao;
+import by.bsuir.pbz2.data.dao.ArtworkExhibitionDao;
+import by.bsuir.pbz2.data.dao.ExhibitionDao;
+import by.bsuir.pbz2.data.dao.impl.ArtworkDaoImpl;
+import by.bsuir.pbz2.data.dao.impl.ArtworkExhibitionDaoImpl;
+import by.bsuir.pbz2.data.dao.impl.ExhibitionDaoImpl;
 import by.bsuir.pbz2.data.entity.Artwork;
 import by.bsuir.pbz2.data.entity.ArtworkExhibition;
 import by.bsuir.pbz2.data.entity.Exhibition;
-import by.bsuir.pbz2.data.dao.impl.ArtworkExhibitionDaoImpl;
 import by.bsuir.pbz2.util.PropertiesManager;
 
 import java.util.List;
@@ -26,27 +30,26 @@ public class MainArtworkExhibition {
         exhibition1.setId((long) 1);
         System.out.println(artworkExhibitionDao.findByExhibitionArtworkId(artwork1, exhibition1));
         System.out.println("================creation=========================");
-        Artwork artwork2 = new Artwork();
-        artwork2.setId((long) 2);
-        artwork2.setTitle("1");
-        Exhibition exhibition2 = new Exhibition();
-        exhibition2.setId((long) 3);
-        exhibition2.setName("1");
+        ArtworkDao artworkDao = getArtworkDao();
+        Artwork artwork2 = artworkDao.findById(3L);
+
+        ExhibitionDao exhibitionDao = getExhibitionDao();
+        Exhibition exhibition2 = exhibitionDao.findById(4L);
+
         ArtworkExhibition artworkExhibition = new ArtworkExhibition();
         artworkExhibition.setArtworkId(artwork2);
         artworkExhibition.setExhibitionId(exhibition2);
-        boolean createdArtworkExhibition = artworkExhibitionDao.create(artworkExhibition);
-        System.out.println(createdArtworkExhibition);
+        artworkExhibitionDao.create(artworkExhibition);
         System.out.println(artworkExhibitionDao.findByExhibitionArtworkId(artwork2, exhibition2));
-        System.out.println("================deleting=========================");
-        Artwork artwork3 = new Artwork();
-        artwork3.setId((long) 2);
-        Exhibition exhibition3 = new Exhibition();
-        exhibition3.setId((long) 3);
-        ArtworkExhibition artworkExhibition2 = new ArtworkExhibition();
-        artworkExhibition2.setArtworkId(artwork3);
-        artworkExhibition2.setExhibitionId(exhibition3);
-        System.out.println(artworkExhibitionDao.delete(artworkExhibition2));
+//        System.out.println("================deleting=========================");
+//        ArtworkDao artworkDao = getArtworkDao();
+//        Artwork artwork3 = artworkDao.findById(1L);
+//        ExhibitionDao exhibitionDao = getExhibitionDao();
+//        Exhibition exhibition3 = exhibitionDao.findById(4L);
+//        ArtworkExhibition artworkExhibition2 = new ArtworkExhibition();
+//        artworkExhibition2.setArtworkId(artwork3);
+//        artworkExhibition2.setExhibitionId(exhibition3);
+//        System.out.println(artworkExhibitionDao.delete(artworkExhibition2));
     }
 
     private static ArtworkExhibitionDao getArtworkExhibitionDao() {
@@ -58,5 +61,27 @@ public class MainArtworkExhibition {
         String driver = propertiesManager.getKey("my.app.db." + profile + ".driver");
         DataSource dataSource = new DataSourceImpl(password, user, url, driver);
         return new ArtworkExhibitionDaoImpl(dataSource);
+    }
+
+    private static ArtworkDao getArtworkDao() {
+        PropertiesManager propertiesManager = new PropertiesManagerImpl("src\\main\\resources\\app.properties");
+        String profile = propertiesManager.getKey("my.app.profile");
+        String url = propertiesManager.getKey("my.app.db." + profile + ".url");
+        String user = propertiesManager.getKey("my.app.db." + profile + ".user");
+        String password = propertiesManager.getKey("my.app.db." + profile + ".password");
+        String driver = propertiesManager.getKey("my.app.db." + profile + ".driver");
+        DataSource dataSource = new DataSourceImpl(password, user, url, driver);
+        return new ArtworkDaoImpl(dataSource);
+    }
+
+    private static ExhibitionDao getExhibitionDao() {
+        PropertiesManager propertiesManager = new PropertiesManagerImpl("src\\main\\resources\\app.properties");
+        String profile = propertiesManager.getKey("my.app.profile");
+        String url = propertiesManager.getKey("my.app.db." + profile + ".url");
+        String user = propertiesManager.getKey("my.app.db." + profile + ".user");
+        String password = propertiesManager.getKey("my.app.db." + profile + ".password");
+        String driver = propertiesManager.getKey("my.app.db." + profile + ".driver");
+        DataSource dataSource = new DataSourceImpl(password, user, url, driver);
+        return new ExhibitionDaoImpl(dataSource);
     }
 }
