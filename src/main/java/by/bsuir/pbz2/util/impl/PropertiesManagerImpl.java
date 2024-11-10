ec81ffc58ed7ddc1aca4170bb7a22850d1226d9d
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-11-10 20:27:46 +0300

    PropertiesManagerImpl.java update

--- src/main/java/by/bsuir/pbz2/util/impl/PropertiesManagerImpl.java
+++ src/main/java/by/bsuir/pbz2/util/impl/PropertiesManagerImpl.java
@@ -2,17 +2,17 @@ package by.bsuir.pbz2.util.impl;
 
 import by.bsuir.pbz2.util.PropertiesManager;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesManagerImpl implements PropertiesManager {
     private final Properties properties;
 
     public PropertiesManagerImpl(String fileName) {
-        try (FileInputStream fis = new FileInputStream(fileName)) {
+        try (InputStream inputStream = getClass().getResourceAsStream(fileName)) {
             properties = new Properties();
-            properties.load(fis);
+            properties.load(inputStream);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
