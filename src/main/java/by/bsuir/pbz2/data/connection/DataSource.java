9058f407ea31bdb93bc4a56b0d51c494c34d4c13
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-11-18 20:10:16 +0300

    Deleted directory connection

--- src/main/java/by/bsuir/pbz2/data/connection/DataSource.java
+++
@@ -1,8 +0,0 @@
-package by.bsuir.pbz2.data.connection;
-
-import java.io.Closeable;
-import java.sql.Connection;
-
-public interface DataSource extends Closeable {
-    Connection getConnection();
-}
