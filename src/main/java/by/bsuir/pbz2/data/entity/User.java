1b6f93858f9345679a1feea81093a6bb13e48fa5
Daniil Pototskiy <daniilpototskiy@mail.ru>
2024-11-19 22:24:43 +0300

    changed field password in User

--- src/main/java/by/bsuir/pbz2/data/entity/User.java
+++ src/main/java/by/bsuir/pbz2/data/entity/User.java
@@ -19,7 +19,7 @@ public class User {
     @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @Column(name = "password")
+    @Column(name = "password", nullable = false)
     private String password;
 
     @Column(name = "created_at", nullable = false)
